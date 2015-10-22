5ae9315d644b32343d625ef9de1359209b2f86c1
EnricoMagnago <en.magnago@gmail.com>
2015-10-22 12:17:54 +0200

    mezza servlet fatto

--- src/java/Servlets/AdminSpettacoli.java
+++ src/java/Servlets/AdminSpettacoli.java
@@ -10,7 +10,13 @@ import Database.DBManager;
 import Database.SpettacoloSalaOrarioQueries;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -39,7 +45,7 @@ public class AdminSpettacoli extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String idSpettacoloStr = request.getParameter("idspettacolo");
-        if(idSpettacoloStr != null){
+        if(idSpettacoloStr != null){ //mostra sala di questo spettacolo.
            /*  int idSpettacolo = Integer.parseInt(idSpettacoloStr);
            InfoPrenotazione infoPrenotazione = null;
             ArrayList<PostiSala> postiSala = null;
@@ -63,16 +69,62 @@ public class AdminSpettacoli extends HttpServlet {
             request.setAttribute("postiSala", postiSala);
             request.setAttribute("prezzi", prezzi);
             request.getRequestDispatcher("/jsp/prenotazione.jsp").forward(request, response);*/
-        }else{
+        }else{ //mega query
             ArrayList<SpettacoloSalaOrario> spettacoli = null;
             SpettacoloSalaOrarioQueries sq = new SpettacoloSalaOrarioQueries(manager);
-//            try {
-//                //TO DO aggiungere film
-//                spettacoli = sq.getSpettacoli();
-//            } catch (SQLException ex) {
-//                request.setAttribute("error", "impossibile caricare la pagina, interrogazione al database fallita");
-//                getServletContext().getRequestDispatcher("/jsp/error.jsp").forward(request, response);
-//            }
+            try {
+                String genereDescrizione = request.getParameter("genere_descrizione");
+                String filmTitolo = request.getParameter("film_titolo");
+                 Integer filmDurataLow;
+                 Integer filmDurataHigh;
+                try{
+                    filmDurataLow = new Integer(request.getParameter("film_durata_low"));
+                    filmDurataHigh = new Integer(request.getParameter("film_durata_high"));
+                }catch(NumberFormatException ex){ //ignore malformed.
+                    filmDurataLow = null;
+                    filmDurataHigh = null;
+                }
+                String filmRegista = request.getParameter("film_regista");
+                DateFormat dateParser = new SimpleDateFormat("dd/MM/yyyy"); //controllare formato data
+                Timestamp spettacoloDataOraLow = null;
+                Timestamp spettacoloDataOraHigh = null;
+                Timestamp prenotazioneDataOraLow = null;
+                Timestamp prenotazioneDataOraHigh = null;
+                try {
+                    String spettacoloDataOraLowString = request.getParameter("spettacolo_data_ora_low");
+                    if(spettacoloDataOraLowString != null)
+                        spettacoloDataOraLow = new Timestamp(dateParser.parse(spettacoloDataOraLowString).getTime());
+                    
+                    String spettacoloDataOraHighString = request.getParameter("spettacolo_data_ora_high");
+                    if(spettacoloDataOraHighString != null)
+                        spettacoloDataOraHigh = new Timestamp(dateParser.parse(spettacoloDataOraHighString).getTime());
+                    
+                    String prenotazioneDataOraLowString = request.getParameter("prenotazione_data_ora_low");
+                    if(prenotazioneDataOraLowString != null)
+                        prenotazioneDataOraLow = new Timestamp(dateParser.parse(prenotazioneDataOraLowString).getTime());
+                    
+                    String prenotazioneDataOraHighString = request.getParameter("prenotazione_data_ora_high");
+                    if(prenotazioneDataOraHighString != null)
+                        prenotazioneDataOraHigh = new Timestamp(dateParser.parse(prenotazioneDataOraHighString).getTime());
+                    
+                } catch (ParseException ex) { //ignore malformed.
+                    spettacoloDataOraLow = null;
+                    spettacoloDataOraHigh = null;
+                    prenotazioneDataOraLow = null;
+                    prenotazioneDataOraHigh = null;
+                }
+                
+                String prezzoTipo = request.getParameter("prezzo_tipo");
+                String salaNome = request.getParameter("sala_nome");
+                Character postoRiga = request.getParameter("posto_riga").charAt(0);
+                Integer postoColonna = new Integer(request.getParameter("posto_colonna"));
+                String utenteEmail = request.getParameter("utente_email");
+                String ruoloRuolo = request.getParameter("ruolo_ruolo");
+                spettacoli = sq.getSpettacoli(genereDescrizione, filmTitolo, filmDurataLow, filmDurataHigh, filmRegista, spettacoloDataOraLow, spettacoloDataOraHigh, prenotazioneDataOraLow, prenotazioneDataOraHigh, prezzoTipo, salaNome, postoRiga, postoColonna, utenteEmail, ruoloRuolo);
+            } catch (SQLException ex) {
+                request.setAttribute("error", "impossibile caricare la pagina, interrogazione al database fallita");
+                getServletContext().getRequestDispatcher("/jsp/error.jsp").forward(request, response);
+            }
             request.setAttribute("spettacoli", spettacoli);
             getServletContext().getRequestDispatcher("/jsp/admin-spettacoli.jsp").forward(request, response);
         }
