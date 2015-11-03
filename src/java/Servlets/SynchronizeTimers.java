c792efee8b6a3ed001163e43dd1186d923d74b62
Stefano <stefano__1994@hotmail.it>
2015-11-03 11:44:17 +0100

    aggiunta servlet che setta timer in PrenotazioneTMP

---
+++ src/java/Servlets/SynchronizeTimers.java
@@ -0,0 +1,84 @@
+package Servlets;
+
+import Beans.PrenotazioneTmp;
+import Database.DBManager;
+import Database.PostoQueries;
+import Database.PrenotazioneTmpQueries;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author roberto
+ */
+public class SynchronizeTimers extends HttpServlet {
+
+    private DBManager manager;
+
+    @Override
+    public void init() throws ServletException {
+        this.manager = (DBManager) super.getServletContext().getAttribute("dbmanager");
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        HttpSession session = request.getSession(false);
+        String idUtente = (String) session.getAttribute("idUtente");
+
+        PrenotazioneTmpQueries ptq = new PrenotazioneTmpQueries(manager);
+        try {
+           ptq.setTimerPrenotazioneTMP(idUtente);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        this.manager = null;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
