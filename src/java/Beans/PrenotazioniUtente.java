7fa10e6753bb741162c47de9f6f8609b356f10ec
Roberto <fellin.roberto@hotmail.it>
2015-11-03 12:10:56 +0100

    servlet e query infoUtente

---
+++ src/java/Beans/PrenotazioniUtente.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author roberto
+ */
+public class PrenotazioniUtente implements Serializable {
+    
+    private Film film;
+    private Timestamp dataOraSpettacolo;
+    private Timestamp dataOraOperazione;
+    private String nomeSala;
+    private int tot; // totale soldi spesi per la prenotazione
+    private int numBiglietti; // totale biglietti acquistati per la prenotazione
+
+    /**
+     * @return the film
+     */
+    public Film getFilm() {
+        return film;
+    }
+
+    /**
+     * @param film the film to set
+     */
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    /**
+     * @return the dataOraSpettacolo
+     */
+    public Timestamp getDataOraSpettacolo() {
+        return dataOraSpettacolo;
+    }
+
+    /**
+     * @param dataOraSpettacolo the dataOraSpettacolo to set
+     */
+    public void setDataOraSpettacolo(Timestamp dataOraSpettacolo) {
+        this.dataOraSpettacolo = dataOraSpettacolo;
+    }
+
+    /**
+     * @return the dataOraOperazione
+     */
+    public Timestamp getDataOraOperazione() {
+        return dataOraOperazione;
+    }
+
+    /**
+     * @param dataOraOperazione the dataOraOperazione to set
+     */
+    public void setDataOraOperazione(Timestamp dataOraOperazione) {
+        this.dataOraOperazione = dataOraOperazione;
+    }
+
+    /**
+     * @return the nomeSala
+     */
+    public String getNomeSala() {
+        return nomeSala;
+    }
+
+    /**
+     * @param nomeSala the nomeSala to set
+     */
+    public void setNomeSala(String nomeSala) {
+        this.nomeSala = nomeSala;
+    }
+
+    /**
+     * @return the tot
+     */
+    public int getTot() {
+        return tot;
+    }
+
+    /**
+     * @param tot the tot to set
+     */
+    public void setTot(int tot) {
+        this.tot = tot;
+    }
+
+    /**
+     * @return the numBiglietti
+     */
+    public int getNumBiglietti() {
+        return numBiglietti;
+    }
+
+    /**
+     * @param numBiglietti the numBiglietti to set
+     */
+    public void setNumBiglietti(int numBiglietti) {
+        this.numBiglietti = numBiglietti;
+    }
+    
+    
+    
+}
---
+++ src/java/Servlets/InfoPrenotazioniUtente.java
@@ -0,0 +1,97 @@
+package Servlets;
+
+import Beans.PrenotazioniUtente;
+import Database.DBManager;
+import Database.UtenteQueries;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class InfoPrenotazioniUtente extends HttpServlet {
+    private DBManager manager;
+
+    
+    
+    @Override
+    public void init() throws ServletException{
+        this.manager = (DBManager)super.getServletContext().getAttribute("dbmanager");
+    }
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         
+        HttpSession session = request.getSession(false);
+        int idUtente=(int)request.getSession(false).getAttribute("idUtente"); //encoded ID.
+        
+        ArrayList<PrenotazioniUtente> infoPrenotazioniUtente = null;
+        UtenteQueries sq = new UtenteQueries(manager);
+        try {
+            infoPrenotazioniUtente = sq.getInfoPrenotazioniUtente(idUtente);
+        } catch (SQLException ex) {
+            request.setAttribute("error", "impossibile caricare la pagina, interrogazione al database fallita");
+            getServletContext().getRequestDispatcher("/jsp/error.jsp").forward(request, response);
+        }
+        request.setAttribute("infoPrenotazioniUtente", infoPrenotazioniUtente);
+        getServletContext().getRequestDispatcher("/jsp/infoPrenotazioniUtente.jsp").forward(request, response);
+  
+    }
+    
+     @Override
+    public void destroy(){
+        this.manager = null;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
